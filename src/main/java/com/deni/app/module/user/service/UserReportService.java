e9cc8c0a0c46e1b5282fb064e636f0ee32066259
Den Paden <denpaden.mail@gmail.com>
2023-02-14 12:25:23 +0700

    create report repo for dummy data

--- src/main/java/com/deni/app/module/user/service/UserReportService.java
+++ src/main/java/com/deni/app/module/user/service/UserReportService.java
@@ -1,13 +1,12 @@
 package com.deni.app.module.user.service;
 
 import com.deni.app.module.user.dto.UserDTO;
+import com.deni.app.module.user.repo.UserReportRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,10 +18,13 @@ public class UserReportService {
     @Autowired
     UserExportToExcelService userExportToExcelService;
 
+    @Autowired
+    UserReportRepo userReportRepo;
+
 
     public void exportToPdf(HttpServletResponse response) throws IOException {
         // get all user
-        List<UserDTO> data = getAllUser();
+        List<UserDTO> data = userReportRepo.getUserList();
 
         // export to pdf
         userExportToPdfService.exportToPDF(response, data);
@@ -31,7 +33,7 @@ public class UserReportService {
 
     public void exportToExcel(HttpServletResponse response) throws IOException {
         // get all user
-        List<UserDTO> data = getAllUser();
+        List<UserDTO> data = userReportRepo.getUserList();
 
         // export to pdf
         userExportToExcelService.exportToExcel(response, data);
@@ -39,19 +41,4 @@ public class UserReportService {
     }
 
 
-    public List<UserDTO> getAllUser() {
-
-        List<UserDTO> list = new ArrayList<>();
-        list.add(UserDTO.builder().id(1L).username("admin").password("*****").roles("ADMIN,USER").permissions("READ,WRITE").active(1).blocked(0).createdDate(new Date()).createdBy("admin").updatedDate(new Date()).updatedBy("admin").build());
-
-        list.add(UserDTO.builder().id(2L).username("user").password("*****").roles("USER").permissions("READ").active(1).blocked(0).createdDate(new Date()).createdBy("admin").updatedDate(new Date()).updatedBy("admin").build());
-
-        list.add(UserDTO.builder().id(3L).username("deni").password("*****").roles("USER").permissions("READ").active(1).blocked(0).createdDate(new Date()).createdBy("admin").updatedDate(new Date()).updatedBy("admin").build());
-
-        list.add(UserDTO.builder().id(4L).username("tating").password("*****").roles("USER").permissions("READ").active(1).blocked(0).createdDate(new Date()).createdBy("admin").updatedDate(new Date()).updatedBy("admin").build());
-
-        list.add(UserDTO.builder().id(5L).username("adnan").password("*****").roles("MANAGER").permissions("READ,WRITE,DROP").active(1).blocked(0).createdDate(new Date()).createdBy("admin").updatedDate(new Date()).updatedBy("admin").build());
-        return list;
-
-    }
 }
